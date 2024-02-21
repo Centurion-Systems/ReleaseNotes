de156abe81d4dcc90879b8b37a061a9c86a6d82e
Christian Strydom <christian.strydom@centurionsystems.co.za>
2024-02-21 09:13:50 +0200

    Update index.html and AppFolder/index.html with new version as well

--- CreateReleaseNotes.py
+++ CreateReleaseNotes.py
@@ -4,6 +4,10 @@ def convert_to_long_date_format(date_str):
     date_obj = datetime.strptime(date_str, '%d/%m/%Y')
     return date_obj.strftime('%B %d, %Y')
 
+def convert_date_format_eu_to_iso(date_str):
+    date_obj = datetime.strptime(date_str, '%d/%m/%Y')
+    return date_obj.strftime('%Y-%m-%d')
+
 def get_list_input(prompt):
     """Prompt the user to enter a list of items, one per line, ending with an empty line."""
     print(prompt)
@@ -16,9 +20,9 @@ def get_list_input(prompt):
     return items
 
 def main():
-    app_name = input("Enter the app name (Ex: MyCentsys Remote): ")
-    app_version = input("Enter the app version (Ex: 1.1.0.79): ")
-    release_date = input("Enter the release date (Ex: 31/01/2024): ")
+    app_name = input("Enter the app name (Ex: MyCentsys Remote): ").strip()
+    app_version = input("Enter the app version (Ex: 1.1.0.79): ").strip()
+    release_date = input("Enter the release date (Ex: 31/01/2024): ").strip()
     new_features = get_list_input("Enter new features (end with an empty line): ")
     improvements = get_list_input("Enter improvements (end with an empty line): ")
 
@@ -103,11 +107,47 @@ def main():
 </body>
 </html>
 """
+    app_folder = f"{app_name.replace(' ','')}"
 
-    file_name = f"{app_name.replace(' ','')}/{app_version}.html"
+    # Create new version file
+    file_name = f"{app_folder}/{app_version}.html"
     with open(file_name, 'w', encoding='utf-8') as file:
         file.write(html_content)
     print(f"Release notes HTML file '{file_name}' has been created.")
+
+    # Update app index.html with new version
+    with open(f"{app_folder}/index.html", 'r', encoding='utf-8') as file:
+        appIndexContent = file.readlines()
+    
+    new_release_info = f"""    <ul>
+        <li><a href="{app_version}.html">{app_version} ({convert_date_format_eu_to_iso(release_date)})</a></li>
+    </ul>"""
+
+    for i, line in enumerate(appIndexContent):
+        if "<h2>Releases</h2>" in line:
+            appIndexContent.insert(i + 1, new_release_info + '\n')
+            break
+
+    with open(f"{app_folder}/index.html", 'w', encoding='utf-8') as file:
+        file.writelines(appIndexContent)
+
+    print(f"Updated file '{app_folder}/index.html'")
+
+    # Update site index.html with new version 
+    index_content_to_replace = f"""                    <h3 style="padding: 0em;">(latest version - v{app_version})</h3>"""
+
+    with open(f"index.html", 'r', encoding='utf-8') as file:
+        indexContent = file.readlines()
+
+    for i, line in enumerate(indexContent):
+        if f"<h2>{app_name}</h2>" in line:
+            indexContent[i + 1] = index_content_to_replace + '\n'
+            break
+
+    with open(f"index.html", 'w', encoding='utf-8') as file:
+        file.writelines(indexContent)
+    
+    print(f"Updated file 'index.html'")
 
 if __name__ == "__main__":
     main()
